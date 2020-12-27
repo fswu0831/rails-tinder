51fe563d8756daf3ea0b2b3f3084951cd4996cf6
Ubuntu <ubuntu@ip-172-31-46-188.us-east-2.compute.internal>
2020-12-27 08:00:42 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, # この行にカンマを追加
-    controllers: { registrations: 'registrations' } # この行を追加
+  devise_for :users, 
+    controllers: { registrations: 'registrations' } 
 
   root 'top#index'
 
